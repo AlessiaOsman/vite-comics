a602511842c45f4cfd88c991269eeadd4c564754
AlessiaOsman <alessia.osman@gmail.com>
2024-02-01 16:40:06 +0100

    added main component

---
+++ src/components/AppMain.vue
@@ -0,0 +1,31 @@
+<script>
+
+export default {
+    name: 'AppMain',
+    data: ()=>({
+        mainText: 'Content goes here'
+    })
+}
+</script>
+
+<template>
+    <main>
+        <div class="container">
+            <p>{{mainText}}</p>
+        </div>
+        
+    </main>
+</template>
+
+<style lang="scss">
+main {
+    background-color: #000000;
+    padding: 32px 0;
+    color: #ffffff;
+
+    p{
+        font-size: 24px;
+    }
+
+}
+</style>
